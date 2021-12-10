ce474ac4a162f0c24d920c822e4c7e1e078b6450
Said <fightingduck20@gmail.com>
2021-12-10 14:55:05 +0300

    add lab1

---
+++ labrab1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	float a;
+scanf("%f" , &a);
+printf("%f" , a);
+char b = (char) a;
+printf("%c", b);
+//printf (" Enter value of A : "); scanf("%f", &a);
+//printf("%f" , a);
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
